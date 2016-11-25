fff05a41be223d10a4915223e837992cb90240c0
Tom Jeanniot <tom.jeanniot@live.fr>
2016-11-25 16:15:19 +0100

    ajout de la fonction delete pour les cocktails

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,5 @@
 class CocktailsController < ApplicationController
-  before_action :find_cocktail, only: [:show]
+  before_action :find_cocktail, only: [:show, :destroy]
 
   def index
     @cocktails = Cocktail.all
@@ -20,6 +20,12 @@ class CocktailsController < ApplicationController
     else
       render "new"
     end
+  end
+
+  def destroy
+    @cocktail.delete
+
+    redirect_to cocktails_path
   end
 
   private
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,4 +1,20 @@
 # encoding: utf-8
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+
+  process eager: true
+
+  process convert: 'jpg'
+
+  version :standard do
+    resize_to_fit 600, 400
+  end
+
+  version :size_index do
+    cloudinary_transformation effect: "brightness:30", width: 100, height: 200, scrop: :scale
+  end
+
+  version :size_show do
+    cloudinary_transformation effect: "brightness:30", width: 200, height: 400, scrop: :scale
+  end
 end
